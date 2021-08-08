ed92d7a41662ffa1dcc85eba89aef1c2cbcee3d9
Bhagwansahay1 <bhagwansahay12110@gmail.com>
2021-08-08 16:00:41 +0530

    minor ui fixes

--- src/sections/carousel.js
+++ src/sections/carousel.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { Typography, Grid } from "@material-ui/core";
 import Styles from "../app-style.js";
-
+import Carousel from "react-material-ui-carousel";
 const DataItem = [
+  {
+    image: ["ClientTras/client1-1.png", "ClientTras/client1-2.png"],
+    name: "Sana Shika",
+    profile: "ClientTras/client1-2.png",
+    text: "had a great experience with Getsetgo and the feeling was mutual. She did everything she was asked for and hence got exactly the same results she signed up for. ",
+  },
+  {
+    image: ["ClientTras/client1-1.png", "ClientTras/client1-2.png"],
+    name: "Sana Shika",
+    profile: "ClientTras/client1-2.png",
+    text: "had a great experience with Getsetgo and the feeling was mutual. She did everything she was asked for and hence got exactly the same results she signed up for. ",
+  },
+  {
+    image: ["ClientTras/client1-1.png", "ClientTras/client1-2.png"],
+    name: "Sana Shika",
+    profile: "ClientTras/client1-2.png",
+    text: "had a great experience with Getsetgo and the feeling was mutual. She did everything she was asked for and hence got exactly the same results she signed up for. ",
+  },
   {
     image: ["ClientTras/client1-1.png", "ClientTras/client1-2.png"],
     name: "Sana Shika",
@@ -21,14 +39,35 @@ const cardStyle = {
 
 const AvtarName = ({ name, profile }) => {
   return (
-    <Grid container item direction="row" justify="flex-start" style={{ marginTop: "32px" }}>
+    <Grid
+      container
+      item
+      direction="row"
+      justify="flex-start"
+      style={{ marginTop: "32px" }}
+    >
       <Grid item container xs={3}>
-        <Grid item style={{ width: "50px", height: "50px",  borderRadius: "50%", backgroundImage: `url(${Styles.backCoverImg(profile)})` }}></Grid>
+        <Grid
+          item
+          style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "50%",
+            backgroundImage: `url(${Styles.backCoverImg(profile)})`,
+          }}
+        ></Grid>
       </Grid>
       <Grid container xs={6} item direction="column">
         {name.split(" ").map((name) => (
           <Grid item>
-            <Typography variant="h4" style={{ maxWidht: "238px" }}>
+            <Typography
+              variant="h5"
+              style={{
+                maxWidht: "238px",
+                ...Styles.colorReef,
+                ...Styles.boldTxt,
+              }}
+            >
               {name}
             </Typography>
           </Grid>
@@ -40,15 +79,45 @@ const AvtarName = ({ name, profile }) => {
 const CarouselContainer = ({ imagePath, text, name, profile }) => {
   return (
     <>
-      <Grid item container direction="row" justify="space-between" alignItems="center" style={{ ...cardStyle }}>
-        <Grid container item xs={6} direction="row" justify="center" style={{ borderRadius: "25% 0", overflow: "hidden" }}>
+      <Grid
+        item
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+        style={{ ...cardStyle }}
+      >
+        <Grid
+          container
+          item
+          xs={6}
+          direction="row"
+          justify="center"
+          style={{ borderRadius: "25% 0", overflow: "hidden" }}
+        >
           {imagePath.map((image) => (
-            <Grid item xs={6} style={{ backgroundRepeat: "no-repeat", height: "360px", width: "100%", backgroundImage: `url(${Styles.backCoverImg(image)})` }}></Grid>
+            <Grid
+              item
+              xs={6}
+              style={{
+                backgroundRepeat: "no-repeat",
+                height: "360px",
+                width: "100%",
+                backgroundImage: `url(${Styles.backCoverImg(image)})`,
+              }}
+            ></Grid>
           ))}
         </Grid>
         <Grid container item xs={5} direction="column" justify="center">
           <Grid item>
-            <Typography variant="h5" style={{ maxWidth: "238px" }}>
+            <Typography
+              variant="h5"
+              style={{
+                maxWidth: "238px",
+                ...Styles.colorCharcoalDark,
+                fontFamily: "Roboto",
+              }}
+            >
               {text}
             </Typography>
           </Grid>
@@ -59,18 +128,30 @@ const CarouselContainer = ({ imagePath, text, name, profile }) => {
   );
 };
 
-const Carousel = () => {
+const CarouselItem = () => {
   return (
     <>
       <Grid item container xs={12} justify="center">
-        {" "}
-        <Grid item container direction="row" justify="center" style={{ maxWidth: "1360px" }}>
-          {DataItem.map(({ image, text, name, profile }) => (
-            <CarouselContainer profile={profile} name={name} imagePath={image} text={text} />
-          ))}
+        <Grid
+          item
+          container
+          direction="row"
+          justify="center"
+          style={{ maxWidth: "1360px" }}
+        >
+          <Carousel interval={5000}>
+            {DataItem.map(({ image, text, name, profile }) => (
+              <CarouselContainer
+                profile={profile}
+                name={name}
+                imagePath={image}
+                text={text}
+              />
+            ))}
+          </Carousel>
         </Grid>
       </Grid>
     </>
   );
 };
-export default Carousel;
+export default CarouselItem;

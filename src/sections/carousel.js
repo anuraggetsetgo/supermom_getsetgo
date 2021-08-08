424e2096b4359c48bb19daa1720c52a62e2b7b4b
Bhagwansahay1 <bhagwansahay12110@gmail.com>
2021-08-08 11:45:07 +0530

    carousel

---
+++ src/sections/carousel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography, Grid } from "@material-ui/core";
+import Styles from "../app-style.js";
+
+const Carousel = () => {
+  return (
+    <>
+      <Grid item container direction="row" justify="center"></Grid>
+    </>
+  );
+};
+export default Carousel;

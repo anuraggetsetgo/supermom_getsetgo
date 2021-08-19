a2fa3843ff1036ce40895e759956b17efce3c560
avn <anurag@getsetgo.fitness>
2021-08-20 00:28:51 +0530

    Pixel

--- src/reactGA.js
+++ src/reactGA.js
@@ -1,11 +1,14 @@
 import ReactGA from "react-ga";
+import {initFBPixel,fbpixel_track_Purchase,fbpixel_viewed_HomePage,fbpixel_clicked_JoinNow,fbpixel_clicked_Payment,fbpixel_payment_Success,fbpixel_payment_failed,fbpixel_appointment_scheduled} from './reactFBPixel'
 //initReactGA,ga_viewed_HomePage,ga_clicked_JoinNow,ga_clicked_Payment,ga_payment_Success,ga_payment_failed,ga_appointment_scheduled
+
 export function initReactGA() {
     ReactGA.initialize("UA-86957619-2");
     ReactGA.pageview(window.location.pathname + window.location.search);
     ReactGA.pageview("/");
     ReactGA.pageview("/trynow");
     //ReactGA.pageview("/summary");
+    initFBPixel();
 }
 export function ga_viewed_HomePage() {
     ReactGA.event({
@@ -13,6 +16,7 @@ export function ga_viewed_HomePage() {
         action: 'Viewed Homepage',
         value: 1,
     });
+    fbpixel_viewed_HomePage();
 }
 
 export function ga_clicked_JoinNow() {
@@ -21,6 +25,7 @@ export function ga_clicked_JoinNow() {
         action: 'Clicked join now',
         value: 2,
     });
+    fbpixel_clicked_JoinNow();
 }
 export function ga_clicked_Payment() {
     ReactGA.event({
@@ -28,6 +33,7 @@ export function ga_clicked_Payment() {
         action: 'Clicked Payment',
         value: 3,
     });
+    fbpixel_clicked_Payment();
 }
 export function ga_payment_Success() {
     ReactGA.event({
@@ -35,6 +41,7 @@ export function ga_payment_Success() {
         action: 'Payment Success',
         value: 5,
     });
+    fbpixel_payment_Success();
 }
 export function ga_payment_failed() {
     ReactGA.event({
@@ -42,6 +49,7 @@ export function ga_payment_failed() {
         action: 'Payment Failed',
         value: 4,
     });
+    fbpixel_payment_failed()
 }
 export function ga_appointment_scheduled() {
     ReactGA.event({
@@ -49,4 +57,5 @@ export function ga_appointment_scheduled() {
         action: 'Scheduled Appointment',
         value: 6,
     });
+    fbpixel_appointment_scheduled();
 }
